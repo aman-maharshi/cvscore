dee4987899b8df54c6c153d973ab7bd62bee7524
aman-maharshi <amanm4harshi@gmail.com>
2025-07-21 00:28:37 +0530

    home init

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home"
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: "CVScore" },
+    { name: "description", content: "CVScore helps you analyze your CV and improve your chances of getting hired." }
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your Applications and Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+      </section>
+    </main>
+  )
 }
