7bb8b95fceb80f728d5713a6bf14047e27ab861c
aman-maharshi <amanm4harshi@gmail.com>
2025-07-21 16:38:05 +0530

    prettier config

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,15 +1,21 @@
+import Navbar from "~/components/navbar"
 import type { Route } from "./+types/home"
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "CVScore" },
-    { name: "description", content: "CVScore helps you analyze your CV and improve your chances of getting hired." }
+    {
+      name: "description",
+      content: "CVScore helps you analyze your CV and improve your chances of getting hired.",
+    },
   ]
 }
 
 export default function Home() {
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
       <section className="main-section">
         <div className="page-heading">
           <h1>Track your Applications and Resume Ratings</h1>
